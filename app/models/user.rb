df713813083ee6224b2ab4fc27589658581c2683
psarma89 <priyam.sarma@gmail.com>
2018-01-19 10:48:49 -0500

    Fixes minor merge leftovers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-<<<<<<< HEAD
 
   validates :username, presence: true, uniqueness: true
-=======
   belongs_to :room
 
->>>>>>> chris/rails-backend
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,20 @@ ActiveRecord::Schema.define(version: 20180119022812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "conversations", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "text"
+    t.bigint "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -28,4 +42,5 @@ ActiveRecord::Schema.define(version: 20180119022812) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "conversations"
 end
